8ae365995843959db5c96af5dd6df1591995a7f5
Gayane Aleksanyan <galexanyan@gmail.com>
2021-05-05 14:01:36 +0400

    various

---
+++ src/com/company/MouseJerry.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class MouseJerry {
+    public static void main(String[] args) {
+        Object obj = new Jerry();
+
+        Mouse mouse = (Mouse) obj;
+        GrayMouse grayMouse = (GrayMouse) mouse;
+        Jerry jerry = (Jerry) grayMouse;
+
+        printClasses(obj, mouse, grayMouse, jerry);
+
+    }
+
+    public static void printClasses(Object obj, Mouse mouse, GrayMouse grayMouse, Jerry jerry) {
+        System.out.println(jerry.getClass().getSimpleName());
+        System.out.println(grayMouse.getClass().getSimpleName());
+        System.out.println(mouse.getClass().getSimpleName());
+        System.out.println(obj.getClass().getSimpleName());
+    }
+
+    static class Mouse {
+    }
+
+    static class GrayMouse extends Mouse {
+    }
+
+    static class Jerry extends GrayMouse {
+    }
+}
---
+++ src/com/company/PlayerDancer.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PlayerDancer {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Person person = null;
+        String key;
+        while (!(key = reader.readLine()).equals("exit")) {
+            if ("player".equals(key)) {
+                person = new Player();
+            } else if ("dancer".equals(key)) {
+                person = new Dancer();
+            }
+            haveFun(person);
+        }
+    }
+
+    public static void haveFun(Person person) {
+        if (person instanceof Player) {
+            ((Player) person).play();
+        } else if (person instanceof Dancer) {
+            ((Dancer) person).dance();
+        }
+    }
+
+    interface Person {
+    }
+
+    static class Player implements Person {
+        void play() {
+            System.out.println("playing");
+        }
+    }
+
+    static class Dancer implements Person {
+        void dance() {
+            System.out.println("dancing");
+        }
+    }
+}
