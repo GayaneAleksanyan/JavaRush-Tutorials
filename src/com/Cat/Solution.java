15c3c588a1149d33160cb5b52e8759b96c94fceb
Gayane Aleksanyan <galexanyan@gmail.com>
2021-05-19 18:01:00 +0400

    revision

---
+++ src/com/Cat/Solution.java
@@ -0,0 +1,23 @@
+package com.Cat;
+
+/* 
+Статики и котики
+*/
+
+public class Solution {
+
+    public static class Cat {
+        public String name;
+    }
+
+    public static Cat cat;
+    static {
+        cat = new Cat();
+        cat.name = "Murka";
+        System.out.println(cat.name);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
