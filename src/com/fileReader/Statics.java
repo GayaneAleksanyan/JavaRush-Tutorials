d7637d3908426a4fffac8f8bc99dfa3d288b327b
Gayane Aleksanyan <galexanyan@gmail.com>
2021-07-27 19:16:13 +0400

    modifier

---
+++ src/com/fileReader/Statics.java
@@ -0,0 +1,7 @@
+package com.fileReader;
+
+public class Statics {
+    public static String FILE_NAME = "C:\\Users\\galex\\OneDrive\\Documents\\test.txt";
+
+    /* add the path to your source file here */
+}
