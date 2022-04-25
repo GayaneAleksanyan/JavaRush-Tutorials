752b559f184f5990ad06383cb5acfc6873ab8788
Gayane Aleksanyan <galexanyan@gmail.com>
2022-04-25 22:07:32 +0400

    wrapper

---
+++ src/com/wrapperSODublicateText/Solution.java
@@ -0,0 +1,35 @@
+package com.wrapperSODublicateText;
+
+import java.io.*;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething();
+        System.setOut(consoleStream);
+
+        String toFile = outputStream.toString();
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        fileOutputStream.write(outputStream.toByteArray());
+
+        System.out.println(toFile);
+        fileOutputStream.close();
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
---
+++ src/com/wrapperSystemOut/Solution.java
@@ -0,0 +1,35 @@
+package com.wrapperSystemOut;
+
+/*
+Ридер обертка
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething();
+
+        String result = outputStream.toString().toUpperCase(Locale.ROOT);
+        System.setOut(consoleStream);
+        System.out.println(result);
+
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
