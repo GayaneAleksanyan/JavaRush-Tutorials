b25ab77b2d67ec54e6f541900303d8ad8caf4273
Gayane Aleksanyan <galexanyan@gmail.com>
2022-05-13 12:59:26 +0400

    revision

---
+++ src/com/wrapperSOAddingLine/Solution.java
@@ -0,0 +1,40 @@
+package com.wrapperSOAddingLine;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething();
+
+        System.setOut(consoleStream);
+
+        String[] result = outputStream.toString().split(System.lineSeparator());
+        int count = 0;
+        for (String line : result) {
+            System.out.println(line);
+            count++;
+            if (count % 2 == 0) {
+                System.out.println("JavaRush - курсы Java онлайн");
+            }
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
