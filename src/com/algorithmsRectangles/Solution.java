ef459184f44fa77cc1ea056ecfffaa318dc373a7
Gayane Aleksanyan <galexanyan@gmail.com>
2022-06-15 14:32:35 +0400

    algorithms

---
+++ src/com/algorithmsRectangles/Solution.java
@@ -0,0 +1,41 @@
+package com.algorithmsRectangles;
+
+/* 
+Алгоритмы-прямоугольники
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        byte[][] a1 = new byte[][]{
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 1}
+        };
+        byte[][] a2 = new byte[][]{
+                {1, 0, 0, 1},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 0, 0, 1}
+        };
+
+        int count1 = getRectangleCount(a1);
+        System.out.println("count = " + count1 + ". Должно быть 2");
+        int count2 = getRectangleCount(a2);
+        System.out.println("count = " + count2 + ". Должно быть 4");
+    }
+
+    public static int getRectangleCount(byte[][] a) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] == 1) {
+                    int down = i + 1 == a.length ? 0 : a[i + 1][j];
+                    int right = j + 1 == a[i].length ? 0 : a[i][j + 1];
+                    if (right == 0 && down == 0) count++;
+                }
+            }
+        }
+        return count;
+    }
+}
